13c270c9d75f09b7daca9b6318cc4fc3f3b5809b
Vladimir Kharitonov <Volodia20.ru@gmail.com>
2021-02-01 21:28:51 +0300

    Add Pooling and Secure

---
+++ Pooling/PoolObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace HaKoLibrary.Pooling
+{
+    public class PoolObject : MonoBehaviour
+    {
+        public PoolContainer PoolContainer { get; set; }
+
+        public virtual void Return()
+        {
+            PoolContainer.Return(this);
+        }
+    }
+}
