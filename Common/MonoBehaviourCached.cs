6f23660a92c15c07fd57eedc35c8e97ba846f261
Vladimir Kharitonov <Volodia20.ru@gmail.com>
2021-02-01 22:37:27 +0300

    Add Scripts Cached Transform

---
+++ Common/MonoBehaviourCached.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace HaKoLibrary.Common
+{
+    public abstract class MonoBehaviourCached : MonoBehaviour
+    {
+        protected Transform _cachedTransform;
+
+        public new Transform transform
+        {
+            get
+            {
+                if ((object)_cachedTransform == null)
+                    _cachedTransform = base.transform;
+
+                return _cachedTransform;
+            }
+        }
+
+        protected virtual void Awake()
+        {
+            if ((object)_cachedTransform == null)
+                _cachedTransform = base.transform;
+        }
+    }
+}
